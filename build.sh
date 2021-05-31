253b3e6ae4820d51922a51368cfecbe9f078087b
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2021-05-31 19:46:59 +0900

    function

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ IMAGE='sshd_server'
 
 C_DIR=$(cd "$(dirname "$0")" || exit 1; pwd)
 
-docker image build "${C_DIR}" --file "${C_DIR}/Dockerfile.14.04" --tag "${IMAGE}":14.04
-docker image build "${C_DIR}" --file "${C_DIR}/Dockerfile.16.04" --tag "${IMAGE}":16.04
-docker image build "${C_DIR}" --file "${C_DIR}/Dockerfile.18.04" --tag "${IMAGE}":18.04
+function build() {
+    # buildx で multi platform な docker image を生成するには事前に docker buildx create --use などしておかないといけないし結構めんどくさい
+    # docker buildx build --platform linux/amd64,linux/arm64 "${C_DIR}" --file "${C_DIR}/Dockerfile.${1}" --tag "${IMAGE}":"${1}"
+    docker image build "${C_DIR}" --file "${C_DIR}/Dockerfile.${1}" --tag "${IMAGE}":"${1}"
+}
+
+build 14.04
+build 16.04
+build 18.04
